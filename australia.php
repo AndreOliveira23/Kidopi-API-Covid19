410d7bb74dd97c2b768bdaeb126fad771ea4d365
André Oliveira <58999171+AndreOliveira23@users.noreply.github.com>
2023-03-20 20:06:39 -0300

    Add Brasil, Canada e Australia.php
    Versões 'estáveis' dos arquivos que retornam os dados de cada país.

---
+++ australia.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>API-Covid-19 - Australia</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+</head>
+<body>
+<?php
+    include('conexao.php');
+    include('footer.php');
+    date_default_timezone_set('America/Sao_Paulo');
+    $data_Atual = date("Y-m-d");
+    $hora_Atual = date("H:i:s");
+    
+    $pais_acessado = "Austrália";
+
+    $url = "https://dev.kidopilabs.com.br/exercicio/covid.php?pais=Australia";
+
+    $dados = json_decode(file_get_contents($url),true);//Trazendo dados da URL para um array associativo
+
+    echo '<table>';
+    echo '<tr>';
+    echo '<td>Estado</td>';
+    echo '<td>Casos confirmados</td>';
+    echo '<td>Mortes confirmadas</td>';
+    echo '</tr>';
+
+    foreach ($dados as $estado) {
+      echo '<tr>';
+      echo '<td>' . $estado["ProvinciaEstado"] . '</td>'; // output first value
+      echo '<td>' . $estado["Confirmados"] . '</td>'; // output second value
+      echo '<td>' . $estado["Mortos"] . '</td>'; // output fourth value
+      echo '</tr>';
+    }
+    echo '</table>';
+    try{
+      
+      $sql = "INSERT INTO acessos (data_acesso, hora_acesso,pais_acessado) VALUES (:data_atual, :hora_atual, :pais_acessado)";            
+      $insert = $conexao->prepare($sql);
+      //Associando valores com placeholders para evitar sql injections
+      $insert->bindValue(':data_atual',$data_Atual, PDO::PARAM_STR); //PARAM_STR = indica que o valor associado é uma String
+      $insert->bindValue(':hora_atual',$hora_Atual, PDO::PARAM_STR);
+      $insert->bindValue(':pais_acessado',$pais_acessado, PDO:: PARAM_STR);
+      $insert->execute();
+      echo "banco de dados atualizado com sucesso!";
+    
+    } catch (PDOException $e){
+      "Erro na inserção: ". $e->getMessage();
+    }
+?>
+
+  <footer>
+      Data do último acesso à API: <?php   echo $data ?> | Último país escolhido para a consulta: <?php echo $pais . "( Antes desse: ".$pais_anterior.")"  ?>
+  </footer>
+<script src="/kidopi/js/main.js">
+
+</script>
+</body>
+</html>
