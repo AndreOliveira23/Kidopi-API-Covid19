9da5732858346d51ac3f37c39f3c01e5c69beb3a
André Oliveira <58999171+AndreOliveira23@users.noreply.github.com>
2023-03-24 23:51:59 -0300

    Update footer.php
    Atualização do arquivo + adição do método footer()

--- footer.php
+++ footer.php
@@ -1,12 +1,19 @@
 <?php
-include('conexao.php');
+    include('conexao.php');
 
-$select = $conexao->prepare("SELECT * FROM acessos ORDER BY id_acesso DESC");
-$select->execute();
-$resultado = $select->fetchAll(PDO::FETCH_ASSOC);
-$data = date("d-m-Y",strtotime($resultado[0]["data_acesso"]));
-$pais = $resultado[0]["pais_acessado"];
-$pais_anterior = $resultado[1]["pais_acessado"];
-$data = str_replace("-",'/',$data);
+    $select = $conexao->prepare("SELECT * FROM acessos WHERE pais_acessado IS NOT NULL ORDER BY id_acesso DESC LIMIT 2");
+    $select->execute();
+    $resultado = $select->fetchAll(PDO::FETCH_ASSOC);
+    $data = date("d-m-Y",strtotime($resultado[0]["data_acesso"]));
+    
+    $pais = str_replace("+"," ",$resultado[0]["pais_acessado"]);
+    
+    function footer(){
+        
+        echo "Data do último acesso à API: ".$GLOBALS["data"]." | Último país consultado: ".$GLOBALS["pais"];
+        
+    }
+    
+    $data = str_replace("-",'/',$data);
 
 ?>
